54579d11f155a06eefda00f2c005d5b22c266d5e
Guilherme Alves <134057697+Guilherme-sta@users.noreply.github.com>
2026-05-27 15:28:58 -0300

    Refactor database schema and insert statements

--- database/database_schema.sql
+++ database/database_schema.sql
@@ -9,7 +9,7 @@ NOME VARCHAR(50) NOT NULL);
 
 CREATE TABLE MARCA(
 ID_MARCA INT NOT NULL PRIMARY KEY,
-ID_FABRICANTE_M INT NOT NULL REFERENCES FABRICANTE_MARCA(ID_FABRICANTE),
+ID_FABRICANTE INT NOT NULL REFERENCES FABRICANTE_MARCA(ID_FABRICANTE),
 NOME VARCHAR(50) NOT NULL);
 
 CREATE TABLE MODELO(
@@ -20,6 +20,7 @@ NOME VARCHAR(50) NOT NULL);
 CREATE TABLE VEICULO(
 ID_VEICULO INT NOT NULL PRIMARY KEY,
 ID_MODELO INT NOT NULL REFERENCES MODELO(ID_MODELO),
+PLACA VARCHAR(7) NOT NULL, 
 ANO INT NOT NULL,
 COR VARCHAR(50) NOT NULL);
 
@@ -31,7 +32,7 @@ COMISSAO FLOAT NOT NULL);
 CREATE TABLE FUNCIONARIO(
 ID_FUNCIONARIO INT NOT NULL PRIMARY KEY,
 ID_CARGO INT NOT NULL REFERENCES CARGO(ID_CARGO),
-CPF VARCHAR(11) NOT NULL,
+CPF VARCHAR(11)  UNIQUE NOT NULL,
 NOME VARCHAR(50) NOT NULL,
 SALARIO FLOAT NOT NULL,
 CONSTRAINT CPF_TAMANHO CHECK (CHAR_LENGTH(CPF) = 11));
@@ -42,13 +43,14 @@ NOME VARCHAR(50) NOT NULL);
 
 CREATE TABLE PECA(
 ID_PECA INT NOT NULL PRIMARY KEY,
-ID_FABRICANTE_P INT NOT NULL REFERENCES FABRICANTE_PECA(ID_FABRICANTE),
+ID_FABRICANTE INT NOT NULL REFERENCES FABRICANTE_PECA(ID_FABRICANTE),
+NOME VARCHAR(50) NOT NULL,
 VALOR_UNITARIO FLOAT NOT NULL,
 ESTOQUE INT NOT NULL);
 
 CREATE TABLE CLIENTE(
 ID_CLIENTE INT NOT NULL PRIMARY KEY,
-CPF VARCHAR(11) NOT NULL,
+CPF VARCHAR(11)  UNIQUE NOT NULL,
 NOME VARCHAR(50) NOT NULL,
 TELEFONE VARCHAR(13) NOT NULL,
 EMAIL VARCHAR(50) NOT NULL,
@@ -72,15 +74,12 @@ ID_POSSE INT NOT NULL REFERENCES POSSE(ID_POSSE),
 ID_SERVICO INT REFERENCES SERVICO(ID_SERVICO),
 ID_PECA INT REFERENCES PECA(ID_PECA),
 DT_ABRE_OS DATE NOT NULL,
-DT_FECHA_OS DATE,
-STATUS VARCHAR(1) CHECK (STATUS = 'A' OR STATUS = 'F')
-VALOR_FINAL FLOAT);
+DT_FECHA_OS DATE NOT NULL,
+STATUS VARCHAR(1) NOT NULL CHECK (STATUS = 'A' OR STATUS = 'F'),
+VALOR_FINAL FLOAT NOT NULL,
+DT_HORA_OS TIMESTAMP DEFAULT NOW());
 
--- MOSTRAR TODAS AS TABELAS
-SELECT table_name 
-FROM information_schema.tables 
-WHERE table_schema='public' 
-AND table_type='BASE TABLE';   
+-- INSERTS
 
 -- FABRICANTE_MARCA
 INSERT INTO FABRICANTE_MARCA (ID_FABRICANTE, NOME) VALUES
@@ -96,40 +95,40 @@ INSERT INTO FABRICANTE_MARCA (ID_FABRICANTE, NOME) VALUES
 (10, 'Fabrica da BYD');
 
 -- MARCA
-INSERT INTO MARCA (ID_MARCA, ID_FABRICANTE_M, NOME) VALUES
-(1, 1, 'Toyota'),        -- Toyota
-(2, 2, 'Volkswagen'),    -- Volkswagen
-(3, 3, 'Chevrolet'),     -- Chevrolet
-(4, 4, 'Volvo'),         -- Volvo
-(5, 5, 'Audi'),          -- Audi
-(6, 6, 'BMW'),           -- BMW
-(7, 7, 'Porsche'),       -- Porsche
-(8, 8, 'Ferrari'),       -- Ferrari
-(9, 9, 'Koenigsegg'),    -- Koenigsegg
-(10, 10, 'BYD');         -- BYD
+INSERT INTO MARCA (ID_MARCA, ID_FABRICANTE, NOME) VALUES
+(1, 1, 'Toyota'),
+(2, 2, 'Volkswagen'),
+(3, 3, 'Chevrolet'),
+(4, 4, 'Volvo'),
+(5, 5, 'Audi'),
+(6, 6, 'BMW'),
+(7, 7, 'Porsche'),
+(8, 8, 'Ferrari'),
+(9, 9, 'Koenigsegg'),
+(10, 10, 'BYD');
 
 -- MODELO
-INSERT INTO MODELO (ID_MODELO, ID_MARCA_M, NOME) VALUES
-(1, 1, 'Corolla'),        -- Toyota
-(2, 1, 'Hilux'),          -- Toyota
-(3, 2, 'Polo'),           -- Volkswagen
-(4, 2, 'Nivus'),          -- Volkswagen
-(5, 3, 'Opala'),          -- Chevrolet
-(6, 3, 'Prisma'),         -- Chevrolet
-(7, 4, 'XC60'),           -- Volvo
-(8, 4, 'XC90'),           -- Volvo
-(9, 5, 'A3'),             -- Audi
-(10, 5, 'Q5'),            -- Audi
-(11, 6, '320i'),          -- BMW
-(12, 6, 'X1'),            -- BMW
-(13, 7, '911'),           -- Porsche
-(14, 7, 'Macan'),         -- Porsche
-(15, 8, 'F8 Tributo'),    -- Ferrari
-(16, 8, 'Roma'),          -- Ferrari
-(17, 9, 'Jesko'),         -- Koenigsegg
-(18, 9, 'Agera RS'),      -- Koenigsegg
-(19, 10, 'Dolphin'),      -- BYD
-(20, 10, 'Seal');         -- BYD
+INSERT INTO MODELO (ID_MODELO, ID_MARCA, NOME) VALUES
+(1, 1, 'Corolla'),
+(2, 1, 'Hilux'),
+(3, 2, 'Polo'),
+(4, 2, 'Nivus'),
+(5, 3, 'Opala'),
+(6, 3, 'Prisma'),
+(7, 4, 'XC60'),
+(8, 4, 'XC90'),
+(9, 5, 'A3'),
+(10, 5, 'Q5'),
+(11, 6, '320i'),
+(12, 6, 'X1'),
+(13, 7, '911'),
+(14, 7, 'Macan'),
+(15, 8, 'F8 Tributo'),
+(16, 8, 'Roma'),
+(17, 9, 'Jesko'),
+(18, 9, 'Agera RS'),
+(19, 10, 'Dolphin'),
+(20, 10, 'Seal');
 
 -- CLIENTE
 INSERT INTO CLIENTE (ID_CLIENTE, CPF, NOME, TELEFONE, EMAIL) VALUES
@@ -155,17 +154,17 @@ INSERT INTO FABRICANTE_PECA (ID_FABRICANTE, NOME) VALUES
 (7, 'Bridgestone');
 
 -- PECA
-INSERT INTO PECA (ID_PECA, ID_FABRICANTE_P, VALOR_UNITARIO, ESTOQUE) VALUES
-(1, 1, 350.00, 10),
-(2, 1, 85.50, 25),
-(3, 2, 190.00, 8),
-(4, 3, 540.00, 5),
-(5, 4, 42.00, 50),
-(6, 5, 65.00, 30),
-(7, 6, 220.00, 12),
-(8, 7, 480.00, 16),
-(9, 2, 115.00, 14),
-(10, 4, 95.00, 40);
+INSERT INTO PECA (ID_PECA,ID_FABRICANTE, NOME, VALOR_UNITARIO, ESTOQUE) VALUES
+(1, 1, 'VELA DE IGNIÇÃO', 350.00, 10),
+(2, 1, 'BATERIA AUTOMATIVA', 85.50, 25),
+(3, 2, 'AMORTECEDOR', 190.00, 8),
+(4, 3, 'BICO INJETOR',540.00, 5),
+(5, 4, 'FILTRO DE ÓLEO', 42.00, 50),
+(6, 5, 'BUCHA DE SUSPENSÃO', 65.00, 30),
+(7, 6, 'DISCO DE FREIO', 220.00, 12),
+(8, 7, 'PNEU', 480.00, 16),
+(9, 2, 'TERMINAL DE DIREÇÃO', 115.00, 14),
+(10, 4, 'PISTÃO DE MOTOR', 95.00, 40);
 
 -- CARGO
 INSERT INTO CARGO (ID_CARGO, NOME, COMISSAO) VALUES
@@ -180,16 +179,16 @@ INSERT INTO CARGO (ID_CARGO, NOME, COMISSAO) VALUES
 
 -- FUNCIONARIO
 INSERT INTO FUNCIONARIO (ID_FUNCIONARIO, ID_CARGO, CPF, NOME, SALARIO) VALUES
-(1, 1, '11111111111', 'Neymar Junior', 4500.00),          -- Mecânico Senior
-(2, 2, '22222222222', 'Lewis Hamilton', 3800.00),         -- Mecânico Pleno
-(3, 3, '33333333333', 'Leon Kennedy', 2800.00),           -- Mecânico Junior
-(4, 4, '44444444444', 'Johnny Joestar', 1800.00),         -- Auxiliar de Mecânico
-(5, 5, '55555555555', 'Vaas Montenegro', 4000.00),        -- Eletricista Automotivo
-(6, 6, '66666666666', 'Carl Johnson', 3200.00),           -- Mecânico Funileiro
-(7, 7, '77777777777', 'Pierre Stardew Valley', 2500.00),  -- Alinhador
-(8, 8, '88888888888', 'Monkey D. Luffy', 2200.00),        -- Borracheiro
-(9, 2, '99999999999', 'Ichigo Kurosaki', 3800.00),        -- Mecânico Pleno
-(10, 1, '00000000000', 'Axl Rose', 4600.00);              -- Mecânico Senior
+(1, 1, '11111111111', 'Neymar Junior', 4500.00),
+(2, 2, '22222222222', 'Lewis Hamilton', 3800.00),
+(3, 3, '33333333333', 'Leon Kennedy', 2800.00),
+(4, 4, '44444444444', 'Johnny Joestar', 1800.00),
+(5, 5, '55555555555', 'Vaas Montenegro', 4000.00),
+(6, 6, '66666666666', 'Carl Johnson', 3200.00),
+(7, 7, '77777777777', 'Pierre Stardew Valley', 2500.00),
+(8, 8, '88888888888', 'Monkey D. Luffy', 2200.00),
+(9, 2, '99999999999', 'Ichigo Kurosaki', 3800.00),
+(10, 1, '00000000000', 'Axl Rose', 4600.00);
 
 -- SERVICO
 INSERT INTO SERVICO (ID_SERVICO, ID_FUNCIONARIO, DESCRICAO, VALOR) VALUES
@@ -205,27 +204,86 @@ INSERT INTO SERVICO (ID_SERVICO, ID_FUNCIONARIO, DESCRICAO, VALOR) VALUES
 (10, 4, 'Limpeza de bicos injetores', 180.00);
 
 -- VEICULO
-INSERT INTO VEICULO (ID_VEICULO, ID_MODELO, ANO, COR) VALUES
-(1, 1, 2023, 'Cinza'),        -- Corolla
-(2, 3, 2020, 'Branco'),       -- Polo
-(3, 5, 1988, 'Preto'),        -- Opala
-(4, 7, 2022, 'Prata'),        -- XC60
-(5, 11, 2021, 'Azul'),        -- BMW 320i
-(6, 13, 2024, 'Vermelho'),    -- Porsche 911
-(7, 19, 2023, 'Branco'),      -- BYD Dolphin
-(8, 2, 2018, 'Prata'),        -- Toyota Hilux
-(9, 15, 2022, 'Amarelo'),     -- Ferrari F8 Tributo
-(10, 10, 2021, 'Preto');      -- Audi Q5
+INSERT INTO VEICULO (ID_VEICULO, ID_MODELO,PLACA, ANO, COR) VALUES
+(1, 1,'NEC5146', 2023, 'Cinza'),
+(2, 3,'MDI0839', 2020, 'Branco'),
+(3, 5,'IHA2714', 1988, 'Preto'),
+(4, 7,'MYO4449', 2022, 'Prata'),
+(5, 11,'LKX9822', 2021, 'Azul'),
+(6, 13,'HQS1905', 2024, 'Vermelho'),
+(7, 19,'NBX9018', 2023, 'Branco'),
+(8, 2,'MVL4749', 2018, 'Prata'),
+(9, 15,'BTR5075', 2022, 'Amarelo'),
+(10, 10,'JYE8823', 2021, 'Preto');
 
--- ORDEM DE SERVIÇO
-INSERT INTO OS (ID_OS, ID_POSSE, ID_SERVICO, ID_PECA, DT_ABRE_OS, DT_FECHA_OS, STATUS, VALOR_FINAL) VALUES
-(1, 1, 1, 1, '2026-05-20', '2026-05-25', 'F', 550.00),    -- Troca de pastilhas/alinhamento + Bosch 350.00
-(2, 2, 7, 5, '2026-05-21', '2026-05-21', 'F', 122.00),    -- Troca de óleo e filtro + Mahle 42.00
-(3, 3, 3, 9, '2026-05-22', '2026-05-24', 'F', 515.00),    -- Alinhamento 3D/pintura + Nakata 115.00
-(4, 4, 6, 6, '2026-05-23', '2026-05-23', 'F', 185.00),    -- Alinhamento 3D + Axios 65.00
-(5, 5, 5, 1, '2026-05-24', '2026-05-25', 'A', 950.00),    -- Revisão 10.000km + Bosch 350.00
-(6, 6, 9, 3, '2026-05-20', '2026-05-22', 'F', 640.00),    -- Troca de kit de embreagem + Nakata 190.00
-(7, 7, 2, 10, '2026-05-22', '2026-05-23', 'F', 445.00),   -- Instalação de kit multimídia + Mahle 95.00
-(8, 8, 4, 8, '2026-05-21', '2026-05-21', 'F', 630.00),    -- Troca de pneus/balanceamento + Bridgestone 480.00
-(9, 9, 8, 4, '2026-05-15', '2026-05-25', 'A', 2040.00),   -- Retífica preventiva de motor + Denso 540.00
-(10, 10, 10, 2, '2026-05-25', '2026-05-25', 'F', 265.50); -- Limpeza de bicos injetores + Bosch 85.50
+-- INSERT POSSE
+INSERT INTO POSSE (ID_POSSE, ID_VEICULO, ID_CLIENTE) VALUES 
+(1, 1, 1),
+(2, 2, 2),
+(3, 3, 3),
+(4, 4, 4),
+(5, 5, 5),
+(6, 6, 6),
+(7, 7, 7),
+(8, 8, 8),
+(9, 9, 9),
+(10, 10, 10);
+
+-- OS
+INSERT INTO OS (ID_OS, ID_POSSE, ID_SERVICO, ID_PECA, DT_ABRE_OS, DT_FECHA_OS, STATUS, VALOR_FINAL,DT_HORA_OS) VALUES
+(1, 1, 1, 1, '2026-05-20', '2026-05-25', 'F', 550.00,DEFAULT),
+(2, 2, 7, 5, '2026-05-21', '2026-05-21', 'F', 122.00,DEFAULT),
+(3, 3, 3, 9, '2026-05-22', '2026-05-24', 'F', 515.00,DEFAULT),
+(4, 4, 6, 6, '2026-05-23', '2026-05-23', 'F', 185.00,DEFAULT),
+(5, 5, 5, 1, '2026-05-24', '2026-05-25', 'A', 950.00,DEFAULT),
+(6, 6, 9, 3, '2026-05-20', '2026-05-22', 'F', 640.00,DEFAULT),
+(7, 7, 2, 10, '2026-05-22', '2026-05-23', 'F', 445.00,DEFAULT),
+(8, 8, 4, 8, '2026-05-21', '2026-05-21', 'F', 630.00,DEFAULT),
+(9, 9, 8, 4, '2026-05-15', '2026-05-25', 'A', 2040.00,DEFAULT),
+(10, 10, 10, 2, '2026-05-25', '2026-05-25', 'F', 265.50,DEFAULT);
+
+-- MOSTRAR TODAS AS TABELAS
+SELECT table_name 
+FROM information_schema.tables 
+WHERE table_schema='public' 
+AND table_type='BASE TABLE';   
+
+-- DELETAR TODAS AS TABELAS
+DROP SCHEMA public CASCADE;
+CREATE SCHEMA public;
+GRANT ALL ON SCHEMA public TO postgres;
+GRANT ALL ON SCHEMA public TO public;
+COMMENT ON SCHEMA public IS 'standard public schema';   
+
+-- SELECTS
+SELECT * FROM VEICULO;
+SELECT * FROM CARGO;
+SELECT * FROM FUNCIONARIO;
+SELECT * FROM FABRICANTE_PECA;
+SELECT * FROM PECA;
+SELECT * FROM FABRICANTE_MARCA;
+SELECT * FROM MARCA;
+SELECT * FROM MODELO;
+SELECT * FROM POSSE;
+SELECT * FROM CLIENTE;
+SELECT * FROM SERVICO;
+SELECT * FROM OS;
+
+-- VIEWS
+
+CREATE OR REPLACE VIEW VISUALIZAR_OS AS
+SELECT ID_OS,C.NOME CLIENTE,C.CPF,M.NOME VEICULO,V.PLACA,V.ANO,V.COR,S.DESCRICAO,F.NOME FABRICANTE,
+PE.NOME PECA,DT_ABRE_OS,COALESCE(CAST((DT_FECHA_OS) AS VARCHAR),'NÃO FOI FECHADA') DT_FECHA_OS,
+STATUS,COALESCE(VALOR_FINAL,0) VALOR_FINAL,DT_HORA_OS
+FROM OS O JOIN PECA PE ON PE.ID_PECA = O.ID_PECA JOIN FABRICANTE_PECA F ON F.ID_FABRICANTE = PE.ID_FABRICANTE 
+JOIN SERVICO S ON O.ID_SERVICO = S.ID_SERVICO JOIN POSSE PO ON O.ID_POSSE = PO.ID_POSSE 
+JOIN CLIENTE C ON PO.ID_CLIENTE = C.ID_CLIENTE JOIN VEICULO V ON PO.ID_VEICULO = V.ID_VEICULO
+JOIN MODELO M ON M.ID_MODELO = V.ID_MODELO;
+
+CREATE OR REPLACE VIEW VISUALIZAR_OS_ABERTAS AS
+SELECT ID_OS,CLIENTE,CPF,VEICULO,PLACA,ANO,COR,DESCRICAO FROM VISUALIZAR_OS
+WHERE STATUS = 'A';
+
+CREATE OR REPLACE VIEW VISUALIZAR_OS_FECHADAS AS
+SELECT ID_OS,CLIENTE,CPF,VEICULO,PLACA,ANO,COR,DESCRICAO FROM VISUALIZAR_OS
+WHERE STATUS = 'F';, 'F', 265.50); 
